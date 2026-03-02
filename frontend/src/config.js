a119855c2837cbf0cebe10712ce55f360d677efb
AleksKey23 <123273754+AleksKey23@users.noreply.github.com>
2026-03-02 09:02:03 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,8 @@
 // Файл конфигурации для настройки API endpoint
 const config = {
-  // Укажите правильный URL вашего бэкенд-сервера
-  API_BASE_URL: 'http://localhost:5000'
+  
+  API_BASE_URL: 'http://45.144.178.89:5000/'
 };
+
 
 export default config;
